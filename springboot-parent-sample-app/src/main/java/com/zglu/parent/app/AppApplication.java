15927e7459435fed026a2ac35546fce5ef292d84
zhigen <385861131@qq.com>
2020-08-19 21:30:17 +0800

    Initial commit

---
+++ springboot-parent-sample-app/src/main/java/com/zglu/parent/app/AppApplication.java
@@ -0,0 +1,16 @@
+package com.zglu.parent.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zglu
+ */
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
---
+++ springboot-parent-sample-common/src/main/java/com/zglu/parent/common/CommonApplication.java
@@ -0,0 +1,16 @@
+package com.zglu.parent.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zglu
+ */
+@SpringBootApplication
+public class CommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+
+}
